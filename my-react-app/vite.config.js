72b34b995d55c45bfea00253120001577cc0e96b
RohanAlwal <rohanalwal1305@gmail.com>
2025-04-21 13:03:18 +0530

    Done

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    define: {
-      __APP_API_URL__: JSON.stringify(process.env.VITE_API_URL || '')
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: process.env.VITE_API_URL || 'http://localhost:5000',
-          changeOrigin: true,
-          secure: false
-        }
-      }
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
 });
